d05ce72d737fcd10c74bb0d15d70c358bbf75f6f
shna rafeeq <shnar.majeed@gmail.com>
2020-05-21 20:47:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 =31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let mod = 104 %10;
+
+let max = Math.max(12,6.3,16,20);
